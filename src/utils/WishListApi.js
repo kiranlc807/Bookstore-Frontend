83c8093c664bca956c5e23e90a60e926218e5608
unknown <kiran807lc@gmail.com>
2024-01-23 14:21:43 +0530

    [Kirana]:Added WishlistApiCall

---
+++ src/utils/WishListApi.js
@@ -0,0 +1,44 @@
+import { Message } from "@mui/icons-material";
+import axios from "axios";
+
+export const GetWishListItems = async()=>{
+    const token = localStorage.getItem('Authorization');
+
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json',
+        },
+    };
+    const res = await axios.get("http://localhost:3000/api/v1/wishlist",config);
+    return res.data.data;
+}
+
+export const AddToWishlist = async (bookId)=>{
+    const token = localStorage.getItem('Authorization');
+
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`,
+            // 'Content-Type': 'application/json',
+        },
+    };
+    console.log(config);
+    const res = await axios.post(`http://localhost:3000/api/v1/wishlist/${bookId}`,null,config)
+    console.log("Addtocart",res.data);
+    return res.data.data;
+}
+
+export const RemoveWishList = async (bookId)=>{
+    const token = localStorage.getItem('Authorization');
+
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`,
+            // 'Content-Type': 'application/json',
+        },
+    };
+    const res = await axios.delete(`http://localhost:3000/api/v1/wishlist/${bookId}`,config)
+    console.log("Api WishList",res);
+    return res.data;
+}
